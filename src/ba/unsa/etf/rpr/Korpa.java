aebfd1ce912d155d35255ae2d2fc1abbb23ddf06
Zlatan <zlatanjakic857@gmail.com>
2021-10-22 17:14:20 +0200

    Commit

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,41 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Korpa {
+    private ArrayList<Artikl> artikli = new ArrayList<Artikl>(0);
+
+    public boolean dodajArtikl(Artikl noviArtikl) {
+        if (artikli.size() < 50) {
+            artikli.add(noviArtikl);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (Artikl a : artikli) {
+            if (a.getKod().equals(kod)) {
+                artikli.remove(a);
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli.toArray(new Artikl[0]);
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijena = 0;
+        for (Artikl a : artikli) {
+            ukupnaCijena += a.getCijena();
+        }
+        return  ukupnaCijena;
+    }
+
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Supermarket {
+    private ArrayList<Artikl> artikli = new ArrayList<Artikl>(0);
+
+    public void dodajArtikl(Artikl a) {
+        artikli.add(a);
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (Artikl a : artikli) {
+            if (a.getKod().equals(kod)) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli.toArray(new Artikl[0]);
+    }
+
+
 }
